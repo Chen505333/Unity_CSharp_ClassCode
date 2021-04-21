507ce9c3b07d55bf5a6829b33b7d1557f14dde7f
宇宸 赵 <1093732236@qq.com>
2021-04-22 01:53:09 +0800

    第一个应用程序

---
+++ CSharp入门/CSharp_Lesson1/Program.cs
@@ -0,0 +1,61 @@
+﻿//第一个应用程序	注释-程序文件的基础结构
+//2021.4.21
+
+//第一种注释 两杠注释
+//第二种注释	星号注释
+//第三种注释 三杠注释
+
+//引用命名空间（工具包）
+using System;
+
+/// <summary>
+/// 命名空间 - 工具包
+/// </summary>
+namespace CSharp_21_4_21
+{
+	//命名空间代码块
+	//面向对象相关知识点内容 会在这里来写代码
+
+	/// <summary>
+	/// 类 - 工具
+	/// </summary>
+	class Program
+	{
+		//类 代码块
+		//面向对象相关知识点内容 会在这里来写代码
+
+		/// <summary>
+		/// 函数 - 工具能做的事情
+		/// 主函数 一个程序的入口
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			//函数代码块 我们目前学习基础知识时
+			//所有代码都会写在该代码块中
+
+			//在控制台打印一行信息 打印结束后自动换行
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("你好世界");
+
+			//在控制台打印一行信息 打印结束后不会自动换行
+			Console.Write("我要好好学习");
+			Console.Write("天天向上");
+
+			//检测玩家输入的代码
+			//等待玩家输入完毕后（按回车键）才会继续执行后面的代码
+			Console.ReadLine();
+
+			//检测玩家是否按键 只要按了键盘上的任意键 就会认为输入结束
+			Console.ReadKey();
+
+			//输入 向控制台输入内容
+			//Console.ReadLine();
+			//Console.ReadKey();
+
+			//输出 在控制台输出信息
+			//Console.WriteLine();
+			//Console.Write();
+		}
+	}
+}
---
+++ CSharp入门/CSharp_Lesson1_exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿//lesson1 习题
+//21.4.21
+
+using System;
+
+namespace Lesson1_exercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Lesson1_exercise");
+
+			//2.
+			//Console.WriteLine("请输入用户名");
+			//Console.ReadLine();
+			//Console.WriteLine("请输入年龄");
+			//Console.ReadLine();
+			//Console.WriteLine("请输入性别");
+			//Console.ReadLine();
+
+			//3.
+			//Console.WriteLine("您喜欢什么运动");
+			//Console.ReadLine();
+			//Console.WriteLine("哈哈，好巧，我也喜欢这个运动");
+
+			//4.
+			Console.WriteLine("**********");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("*        *");
+			Console.WriteLine("**********");
+		}
+	}
+}
